9e07c43812e5369aa554fd968adb4019dfbb173c
Monika Wieczorek <monikawieczoreq@gmail.com>
2020-06-05 18:59:54 +0200

    Add changing dots

--- js/script.js
+++ js/script.js
@@ -13,9 +13,16 @@ const slideList = [{
 
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
+const dots = [...document.querySelectorAll('.dots span')];
 
 const time = 3000;
 let active = 0;
+
+const changeDot = () => {
+  const activeDot = dots.findIndex(dot => dot.classList.contains('active'));
+  dots[activeDot].classList.remove('active');
+  dots[active].classList.add('active');
+}
 
 const changeSlide = () => {
   active++;
@@ -25,6 +32,7 @@ const changeSlide = () => {
 
   image.src = slideList[active].img;
   h1.textContent = slideList[active].text;
+  changeDot()
 }
 
 setInterval(changeSlide, time);
